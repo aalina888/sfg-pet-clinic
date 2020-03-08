6b695e8b95ba83e8a8d651991d8c48b4a2ee8b8c
aalina888 <alinaarap@gmail.com>
2020-03-08 15:45:01 +0200

    Map Base Services Implemented. Closes #6.

---
+++ pet-clinic-data/src/main/java/com/aalina/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.aalina.sfgpetclinic.services.map;
+
+import com.aalina.sfgpetclinic.model.Vet;
+import com.aalina.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteByID(id);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
