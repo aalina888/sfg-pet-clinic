75c4dc496166b40f98ea681707bba587abce2546
aalina888 <alinaarap@gmail.com>
2020-03-18 09:43:46 +0200

    Vets Converted to JPA Entities. Closes #37.

--- pet-clinic-data/src/main/java/com/aalina/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/aalina/sfgpetclinic/model/Specialty.java
@@ -3,8 +3,13 @@ package com.aalina.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
     private String description;
 }
