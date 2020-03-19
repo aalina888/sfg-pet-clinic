76fb80e8d22aa5a8228622cb9d0ce83032d3a72e
aalina888 <alinaarap@gmail.com>
2020-03-19 16:18:11 +0200

    More Lombok Added. Closes #51.

--- pet-clinic-data/src/main/java/com/aalina/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/aalina/sfgpetclinic/model/Specialty.java
@@ -1,13 +1,15 @@
 package com.aalina.sfgpetclinic.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialties")
 public class Specialty extends BaseEntity {
