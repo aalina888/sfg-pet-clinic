72f2b2aa1dbb2e617f026d3f9fae826ada84a90c
aalina888 <alinaarap@gmail.com>
2020-03-19 13:04:32 +0200

    Profiles Updated to Use Spring Data JPA Services. Closes #48.

--- pet-clinic-data/src/main/java/com/aalina/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/aalina/sfgpetclinic/services/map/OwnerMapService.java
@@ -5,11 +5,13 @@ import com.aalina.sfgpetclinic.model.Pet;
 import com.aalina.sfgpetclinic.services.OwnerService;
 import com.aalina.sfgpetclinic.services.PetService;
 import com.aalina.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
     private final PetTypeService petTypeService;
     private final PetService petService;
